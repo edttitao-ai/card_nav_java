21ae648e0f63283e3ef0d8ebff2b2f06aa177175
edttitao-ai <edttitao@gmail.com>
2026-07-03 16:42:04 +0800

    1、修改ai配置的变量 2、把ai响应的内容，改成json格式返回前端，然后示例代码才有换行符

--- src/main/java/com/tao/card_nav/controller/AiChatController.java
+++ src/main/java/com/tao/card_nav/controller/AiChatController.java
@@ -1,12 +1,17 @@
 package com.tao.card_nav.controller;
 
 
+import cn.hutool.json.JSONUtil;
 import com.tao.card_nav.ai.aiService.AiServiceAssistant;
 
 import jakarta.annotation.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -15,8 +20,22 @@ public class AiChatController {
     @Resource
     private AiServiceAssistant aiService;
 
-    @PostMapping( produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> chat(@RequestBody String userMessage) {
-        return aiService.chatNav(userMessage);
+    @PostMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> chat(@RequestBody String userMessage) {
+        return aiService.chatNav(userMessage)
+                .map(chunk -> {
+                    Map<String, String> wrapper = Map.of("d", chunk);
+                    String jsonData = JSONUtil.toJsonStr(wrapper);
+                    return ServerSentEvent.<String>builder()
+                            .data(jsonData)
+                            .build();
+                })
+                .concatWith(Mono.just(
+                        // 发送结束事件
+                        ServerSentEvent.<String>builder()
+                                .event("done")
+                                .data("")
+                                .build()
+                ));
     }
 }
